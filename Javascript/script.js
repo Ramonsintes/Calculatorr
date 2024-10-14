20d80e31ff1bdcf5bd0e4a43bc0e794f718f3a88
Ramonsintes <rsintes20928@iesjoanramis.org>
2024-10-14 10:40:56 +0200

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,33 @@
+
+
+function suma(){
+    let n1 = parseFloat(prompt("Dime un num"));
+let n2 = parseFloat(prompt("Dime otro num"))
+document.write(n1+n2)
+
+}
+
+function resta(){
+    let n1 = parseFloat(prompt("Dime un num"));
+let n2 = parseFloat(prompt("Dime otro num"))
+document.write(n1-n2)
+
+}
+function multiplicacio(){
+    let n1 = parseFloat(prompt("Dime un num"));
+let n2 = parseFloat(prompt("Dime otro num"))
+document.write(n1*n2)
+
+}
+function divisio(){
+    let n1 = parseFloat(prompt("Dime un num"));
+let n2 = parseFloat(prompt("Dime otro num"))
+document.write(n1/n2)
+
+}
+function arrel(){
+    let n1 = parseFloat(prompt("Dime un num"));
+let n2 = parseFloat(prompt("Dime otro num"))
+document.write(Math.sqrt(n1))
+
+}
